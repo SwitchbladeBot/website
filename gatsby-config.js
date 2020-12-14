ebfda9793e6dbf0013518a9ed7a24cb4dd65b88d
pedrofracassi <pedroluizfracassi@gmail.com>
2020-12-14 00:09:42 -0300

    yeet sitemap for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-layout"
